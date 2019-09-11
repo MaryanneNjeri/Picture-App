ce253e42912720858e87de720a04ee1b21debe50
maryanne <mary.jereh@gmail.com>
2019-09-11 16:32:00 +0300

    fixing gitignore file

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-import * as firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: ' AIzaSyBXUQiVw5-GwVR9kngHyRhdW_H0xlmacDM ',
-  authDomain: 'pictureapp-8f523.firebasepp.com',
-  databaseURL: 'https://pictureapp-8f523.firebaseio.com/',
-  storageBucket: 'pictureapp-8f523.appspot.com',
-};
-firebase.initializeApp(firebaseConfig);
